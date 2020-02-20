28af3b73b2aea293cfd0e67f23e5d1b75638195f
Jiahua <tangjiahua@bit.edu.cn>
2020-02-20 19:42:29 +0800

    v 1.0

---
+++ ImageBrowser/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ImageBrowser
+//
+//  Created by 汤佳桦 on 2020/2/20.
+//  Copyright © 2020 bitsocialgroup.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let ScreenFrame = UIScreen.main.bounds
+    let ScreenWidth = UIScreen.main.bounds.size.width
+    let ScreenHeight = UIScreen.main.bounds.size.height
+    let SpaceWidth:CGFloat = 20
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        initView()
+    }
+
+    
+    func initView(){
+        
+    }
+    
+}
+
